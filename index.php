30f260ba2788ee0637fe77e4ce77f29ea3a64207
james <jjasugan@gmail.com>
2025-02-06 17:32:36 +0800

    flowbite removed

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?? "Document"; ?></title>
+    <!-- css -->
     <link rel="stylesheet" href="src/css/style.css">
-    <link rel="stylesheet" href="node_modules/flowbite/dist/flowbite.min.css">
     <link rel="stylesheet" href="src/css/output.css">
+
     <script src="https://kit.fontawesome.com/70df29d299.js" crossorigin="anonymous"></script>
     <script defer src="src/jquery/dist/jquery.min.js"></script>
     <link rel="stylesheet" href="src/css/swiper/swiper-bundle.min.css"/>
@@ -22,6 +23,5 @@
 
 
 
-    <script src="node_modules/flowbite/dist/flowbite.min.js"></script>
 </body>
 </html> 
--- src/php/services.php
+++ src/php/services.php
@@ -1,3 +1,6 @@
-<section id="services" class="w-full bg-white py-20">
-    <div></div>
+<section id="services" class="w-full h-screen bg-white py-20 text-black">
+    <div class="max-w-screen-lg mx-auto px-10">
+        <h1 class="text-lg sm:text-7xl  font-bold text-center">Top Values for you</h1>
+        <p>Try variety of benefits when using our services</p>
+    </div>
 </section>
